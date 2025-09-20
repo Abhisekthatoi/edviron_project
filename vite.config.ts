f454b10c9ab5e83151e98938b2bf493cc3410e9e
Abhisek Thatoi <71206215+Abhisekthatoi@users.noreply.github.com>
2025-09-20 14:01:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: { port: 5173 }
+  server: {
+    host: true,  // allows access from outside container
+    port: 5173
+  }
 });
